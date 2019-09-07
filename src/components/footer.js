689bba71afc91d8af501ebac53c57e0859af0725
Jeremy Hamm <jeremythamm@gmail.com>
2019-09-07 16:11:02 -0700

    Add personal link and update flex spacing on thumbnails

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/Footer.scss';
 function Footer() {
-    return (
-      <div className="Footer">
-        <p className="copyright">&copy; {new Date().getFullYear()}</p>
-      </div>
-    );
-  }
+  return (
+    <div className="Footer">
+      <p className="copyright">&copy; {new Date().getFullYear()}</p>
+      <p className="name"><a href="https://uphere.space">Jeremy Hamm</a></p>
+    </div>
+  );
+}
   
-  export default Footer;
+export default Footer;
   
