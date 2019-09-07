807cf58736dff1dd3d79653e073bf3eb63a3f809
Jeremy Hamm <jeremythamm@gmail.com>
2019-09-07 16:29:10 -0700

    Update slider chevron style

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -19,8 +19,12 @@ class Carousel extends Component {
         {planets}
       </div>
       <div className="carousel-links">
-        <img onClick={() => this.activateLeftSlider()} src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBkPSJNMTQuMTkgMTYuMDA1bDcuODY5IDcuODY4LTIuMTI5IDIuMTI5LTkuOTk2LTkuOTk3TDE5LjkzNyA2LjAwMmwyLjEyNyAyLjEyOXoiLz48L3N2Zz4='/>
-        <img onClick={() => this.activateRightSlider()} src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBkPSJNMTguNjI5IDE1Ljk5N2wtNy4wODMtNy4wODFMMTMuNDYyIDdsOC45OTcgOC45OTdMMTMuNDU3IDI1bC0xLjkxNi0xLjkxNnoiLz48L3N2Zz4='/>
+        <div onClick={() => this.activateLeftSlider()}>
+          <span class="chevron left"></span>
+        </div>
+        <div onClick={() => this.activateRightSlider()}>
+          <span class="chevron right"></span>
+        </div>
       </div>
     </div>
   }
