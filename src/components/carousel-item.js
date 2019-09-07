284ec367bf30f9da37c506a327b6565760e72e59
Jeremy Hamm <jeremythamm@gmail.com>
2019-09-06 19:04:03 -0700

    Update carousel

--- src/components/carousel-item.js
+++ src/components/carousel-item.js
@@ -11,7 +11,7 @@ const PlanetItem = (props) => {
 
 const Image = (props) => {
   return(
-    <img src={process.env.REACT_APP_PUBLIC_URL + '/images/' + props.data + '.jpg'} alt="Planet" className="thumbnail" height="150" width="150"></img>
+    <img src={process.env.REACT_APP_PUBLIC_URL + '/images/' + props.data + '.jpg'} alt="Planet" className="thumbnail" height="250" width="250"></img>
   )
 }
 
--- src/components/carousel.js
+++ src/components/carousel.js
@@ -4,13 +4,23 @@ import planetData from '../assets/json/planets.json';
 import { CarouselItem }  from './carousel-item.js';
 
 class Carousel extends Component {
+  activateLeftSlider = () => {
+    this.refs.carousel.scrollLeft -= 300;
+  }
+  activateRightSlider = () => {
+    this.refs.carousel.scrollLeft += 300;
+  }
   render() {
     const planets = planetData.map(data => {
-      return <CarouselItem key={data.id} planet={data} />
+      return <CarouselItem id={data.id} key={data.id} planet={data} />
     });
     return <div className="carousel-container">
-      <div className="carousel">
+      <div className="carousel" ref="carousel"ref="carousel">
         {planets}
+      </div>
+      <div className="carousel-links">
+        <img onClick={() => this.activateLeftSlider()} src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBkPSJNMTQuMTkgMTYuMDA1bDcuODY5IDcuODY4LTIuMTI5IDIuMTI5LTkuOTk2LTkuOTk3TDE5LjkzNyA2LjAwMmwyLjEyNyAyLjEyOXoiLz48L3N2Zz4='/>
+        <img onClick={() => this.activateRightSlider()} src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBkPSJNMTguNjI5IDE1Ljk5N2wtNy4wODMtNy4wODFMMTMuNDYyIDdsOC45OTcgOC45OTdMMTMuNDU3IDI1bC0xLjkxNi0xLjkxNnoiLz48L3N2Zz4='/>
       </div>
     </div>
   }
